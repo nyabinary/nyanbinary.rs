85ca478ec582d99aabc328a06271ec0a519666bb
Nyabinary <97130632+Nyabinary@users.noreply.github.com>
2023-09-30 15:21:52 +0000

    Deploying to gh-pages from @ Nyabinary/nyanbinary.rs@c43e4b2a037d19723d0c33c909cf06d359d0cd81 🚀

--- nyanbinary-rs-968513d7deb76969.js
+++ nyanbinary-rs-968513d7deb76969.js
@@ -514,7 +514,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('nyanbinary-rs-735725d0d9b266b5_bg.wasm', import.meta.url);
+        input = new URL('nyanbinary-rs-968513d7deb76969_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
